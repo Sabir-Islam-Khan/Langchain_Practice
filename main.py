16c67a19faf6d590e00f4864a2e3a81b550b1ca8
Sabir Islam Khan <Sabir.islam14@gmail.com>
2024-02-14 03:30:25 +0600

    update

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import streamlit as st
 
 st.title("Car Suggestion LangChain")
 st.markdown("<div style='margin-bottom:20px'></div>", unsafe_allow_html=True)
-st.selectbox("What type of car you like?", ("German", "Japaneese", "Italian", "American", "French"))
+car_make = st.selectbox("What type of car you like?", ("German", "Japanese", "Italian", "American", "French"))
 st.markdown("<div style='margin-bottom:10px'></div>", unsafe_allow_html=True)
-st.selectbox("What is your budget range?", ("10000$-20000$", "25000$-40000$", "70000$-10000$", "More than 100000$"))
+budget = st.selectbox("What is your budget range?", ("10000$-20000$", "25000$-40000$", "70000$-100000$", "More than 100000$"))
 
+is_searched = st.button(label="Search")
+
+if is_searched:
+    response = helper.suggest_cars(car_make, budget)
+    with st.container():
+        st.write(response["text"], type="info")

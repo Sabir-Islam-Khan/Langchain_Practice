fa0012194d06d2c47b25e0bfbe3984270ce6f8ef
Sabir Islam Khan <Sabir.islam14@gmail.com>
2024-02-14 02:35:50 +0600

    helper class

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
-from langchain.llms import OpenAI 
-from dotenv import load_dotenv
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+import langchain_helper as helper 
+import streamlit as st 
 
-load_dotenv()
+st.title("Car Suggestion LangChain")
+st.markdown("<div style='margin-bottom:20px'></div>", unsafe_allow_html=True)
+st.selectbox("What type of car you like?", ("German", "Japaneese", "Italian", "American", "French"))
+st.markdown("<div style='margin-bottom:10px'></div>", unsafe_allow_html=True)
+st.selectbox("What is your budget range?", ("10000$-20000$", "25000$-40000$", "70000$-10000$", "More than 100000$"))
 
-def suggest_cars(car_type, budget):
-    llm = OpenAI(temperature=0.7)
-
-    promptTemplate = PromptTemplate(
-        input_variables=["car_type","budget",],
-        template="I love cars. I want to buy a car. Suggest me 5 cars of {car_type} and my budget is {budget}" )
-    
-    name_chain = LLMChain(llm=llm, prompt=promptTemplate)
-    
-    response = name_chain({"car_type": car_type, "budget":budget})
-    return response
-
-if __name__ == "__main__":
-    type = input("Enter preferred car type:\n")
-    budget = input("Enter your budget range:\n")
-    print("\n")
-    print(suggest_cars(type,budget))

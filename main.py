a3cf4421f39dc982aba920e5254286e68ee84a90
White-Wolf007 <160585170+White-Wolf007@users.noreply.github.com>
2024-02-20 22:16:09 +0600

    UI

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import langchain_helper as helper 
 import streamlit as st 
 
-st.title("Car Suggestion LangChain")
-st.markdown("<div style='margin-bottom:20px'></div>", unsafe_allow_html=True)
+st.title("Suggest me vroom!")
+st.markdown("<div style='margin-bottom:30px'></div>", unsafe_allow_html=True)
 car_make = st.selectbox("What type of car you like?", ("German", "Japanese", "Italian", "American", "French"))
 st.markdown("<div style='margin-bottom:10px'></div>", unsafe_allow_html=True)
 budget = st.selectbox("What is your budget range?", ("10000$-20000$", "25000$-40000$", "70000$-100000$", "More than 100000$"))
